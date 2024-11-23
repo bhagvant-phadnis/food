30ffe13809c863db1bd51bd1924b57d2c801cc3d
bhagvant-phadnis <bhagvant.phadnis@gmail.com>
2024-11-23 20:25:23 +0100

    static files and template redering base to index page

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://plakarestaurant.ca/wp-content/themes/twentytwentythree-child/img/food-placeholder.png")
 
     def __str__(self):
         return self.item_name
--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'food'
+
 urlpatterns = [
     #/food/
     path('', views.index, name='index'),
