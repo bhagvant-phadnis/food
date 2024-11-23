6ad330ee8939ea9de930d5a25d654a26085810a0
bhagvant-phadnis <bhagvant.phadnis@gmail.com>
2024-11-23 12:24:08 +0100

    Adding models, templates & modified views

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #/food/
     path('', views.index, name='index'),
+    #/food/id
+    path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
 ]
